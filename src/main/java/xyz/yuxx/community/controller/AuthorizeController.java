6c5aa1e060fa451a29c31986b74e0fcab740a1cb
Hyxx_cx <xiechxin@163.com>
2019-12-14 12:21:42 +0800

    update properties

--- src/main/java/xyz/yuxx/community/controller/AuthorizeController.java
+++ src/main/java/xyz/yuxx/community/controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package xyz.yuxx.community.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,18 +16,24 @@ public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
 
+    @Value("${github.redirect.uri}")
+    private String redirect_uri;
+    @Value("${github.client.secret}")
+    private String client_secret;
+    @Value("${github.client.id}")
+    private String client_id;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(value = "code") String code,
                            @RequestParam(value = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
-        accessTokenDTO.setClient_id("73b35e5a3e56e148322c");
-        accessTokenDTO.setClient_secret("cd62b18b7044c8aefebe67f5c8e428a62418fb16");
+        accessTokenDTO.setRedirect_uri(redirect_uri);
+        accessTokenDTO.setClient_id(client_id);
+        accessTokenDTO.setClient_secret(client_secret);
         accessTokenDTO.setState(state);
-        String access_token = gitHubProvider.getAccessToken(accessTokenDTO);
-        GitHubUser gitHubUser = gitHubProvider.getGitHubUser(access_token);
+        String access_token = gitHubProvider.getAccessToken(accessTokenDTO);//获取access_token
+        GitHubUser gitHubUser = gitHubProvider.getGitHubUser(access_token);//获取user信息
         System.out.println(gitHubUser.getId());
         System.out.println(gitHubUser.getName());
         System.out.println(gitHubUser.getBio());
--- src/main/java/xyz/yuxx/community/provider/GitHubProvider.java
+++ src/main/java/xyz/yuxx/community/provider/GitHubProvider.java
@@ -16,22 +16,26 @@ public class GitHubProvider {
         MediaType mediaType = MediaType.get("application/json; charset=utf-8");
 
         OkHttpClient client = new OkHttpClient();
-
-
-            RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO) );
-            Request request = new Request.Builder()
-                    .url("https://github.com/login/oauth/access_token")
-                    .post(body)
-                    .build();
-            try (Response response = client.newCall(request).execute()) {
-                String string = response.body().string();
-                String access_token = string.split("=")[1].split("&")[0];
-                System.out.println(access_token);
-                return access_token;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
+        /**
+         * 使用okhttp发送一个post请求，请求的参数存放在accessTokenDTO这个实体类中
+         * 通过toJSONString将实体类转换成json样式的字符串。json就是类似map那种
+         * json样式字符串就长这样{"a": "Hello", "b": "World"}，键值一一对应
+         */
+        RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO) );
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();//就是获得响应的字符串类型数据
+            String access_token = string.split("=")[1].split("&")[0];
+            //根据拿到的原始accesstoken字符串分裂得到我们要的access_token
+            System.out.println(access_token);
+            return access_token;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
 
     }
 
@@ -42,9 +46,9 @@ public class GitHubProvider {
                 .build();
         try {
             Response response = client.newCall(request).execute();
-            String string = response.body().string();
+            String string = response.body().string();//获取到json类型的数据
             System.out.println(string);
-            GitHubUser gitHubUser = JSON.parseObject(string, GitHubUser.class);
+            GitHubUser gitHubUser = JSON.parseObject(string, GitHubUser.class);//将json数据保存到实体类中
             return  gitHubUser;
         } catch (IOException e) {
             e.printStackTrace();
